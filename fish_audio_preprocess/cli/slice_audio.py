e1a090dc1d6bfddff9975ec8f4ba5635c83b5001
AnyaCoder <a2983352531@outlook.com>
2024-10-28 19:23:05 +0800

    slice audio v3 - silero vad

--- fish_audio_preprocess/cli/slice_audio.py
+++ fish_audio_preprocess/cli/slice_audio.py
@@ -289,5 +289,140 @@ def slice_audio_v2(
     logger.info(f"Output directory: {output_dir}")
 
 
+@click.command()
+@click.argument("input_dir", type=click.Path(exists=True, file_okay=False))
+@click.argument("output_dir", type=click.Path(exists=False, file_okay=False))
+@click.option("--recursive/--no-recursive", default=True, help="Search recursively")
+@click.option(
+    "--overwrite/--no-overwrite", default=False, help="Overwrite existing files"
+)
+@click.option(
+    "--clean/--no-clean", default=False, help="Clean output directory before processing"
+)
+@click.option(
+    "--num-workers",
+    help="Number of workers to use for processing, defaults 1",
+    default=1,
+    show_default=True,
+    type=int,
+)
+@click.option(
+    "--min-duration",
+    help="Minimum duration of each slice",
+    default=0.5,
+    show_default=True,
+    type=float,
+)
+@click.option(
+    "--max-duration",
+    help="Maximum duration of each slice",
+    default=20.0,
+    show_default=True,
+    type=float,
+)
+@click.option(
+    "--min-silence-duration",
+    help="Minimum duration of each slice",
+    default=0.3,
+    show_default=True,
+    type=float,
+)
+@click.option(
+    "--speech-pad-duration",
+    help="final speech chunks are padded by speech pad duration(s) each side",
+    default=0.1,
+    show_default=True,
+    type=float,
+)
+@click.option(
+    "--flat-layout/--no-flat-layout", default=False, help="Use flat directory structure"
+)
+@click.option(
+    "--merge-short/--no-merge-short",
+    default=False,
+    help="Merge short slices automatically",
+)
+def slice_audio_v3(
+    input_dir: str,
+    output_dir: str,
+    recursive: bool,
+    overwrite: bool,
+    clean: bool,
+    num_workers: int,
+    min_duration: float,
+    max_duration: float,
+    min_silence_duration: float,
+    speech_pad_duration: float,
+    flat_layout: bool,
+    merge_short: bool,
+):
+    """(silero-vad version) Slice audio files into smaller chunks by silence."""
+
+    from fish_audio_preprocess.utils.slice_audio_v3 import slice_audio_file_v3
+
+    input_dir, output_dir = Path(input_dir), Path(output_dir)
+
+    if flat_layout:
+        logger.info("Using flat directory structure")
+
+    if merge_short:
+        logger.info("Merging short slices automatically")
+
+    if input_dir == output_dir and clean:
+        logger.error("You are trying to clean the input directory, aborting")
+        return
+
+    make_dirs(output_dir, clean)
+
+    files = list_files(input_dir, extensions=AUDIO_EXTENSIONS, recursive=recursive)
+    logger.info(f"Found {len(files)} files, processing...")
+
+    skipped = 0
+
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        tasks = []
+
+        for file in tqdm(files, desc="Preparing tasks"):
+            # Get relative path to input_dir
+            relative_path = file.relative_to(input_dir)
+            save_path = output_dir / relative_path.parent / relative_path.stem
+
+            if save_path.exists() and not overwrite:
+                skipped += 1
+                continue
+
+            if (
+                output_dir / relative_path.parent / relative_path.stem
+                if not flat_layout
+                else output_dir / relative_path.parent
+            ).exists() is False:
+                (
+                    output_dir / relative_path.parent / relative_path.stem
+                    if not flat_layout
+                    else output_dir / relative_path.parent
+                ).mkdir(parents=True)
+
+            tasks.append(
+                executor.submit(
+                    slice_audio_file_v3,
+                    input_file=str(file),
+                    output_dir=save_path,
+                    min_duration=min_duration,
+                    max_duration=max_duration,
+                    min_silence_duration=min_silence_duration,
+                    speech_pad_s=speech_pad_duration,
+                    flat_layout=flat_layout,
+                    merge_short=merge_short,
+                )
+            )
+
+        for i in tqdm(as_completed(tasks), total=len(tasks), desc="Processing"):
+            assert i.exception() is None, i.exception()
+
+    logger.info("Done!")
+    logger.info(f"Total: {len(files)}, Skipped: {skipped}")
+    logger.info(f"Output directory: {output_dir}")
+
+
 if __name__ == "__main__":
-    slice_audio()
+    slice_audio_v3()
